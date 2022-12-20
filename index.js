6998154c42e34636ed38d21ab39a9290a127c659
lx <37706103+helunxing@users.noreply.github.com>
2022-12-20 00:36:19 +0000

    heroku setting

---
+++ index.js
@@ -0,0 +1,13 @@
+const PORT = process.env.PORT || 5001
+const http = require('http');
+const hostname = '127.0.0.1';
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World');
+});
+
+server.listen(PORT, hostname, () => {
+    console.log(`Server running at http://${hostname}:${PORT}/`);
+});
