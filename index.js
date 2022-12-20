6a76756a3acf42e496b6ded142b194ee45781bb7
lx <37706103+helunxing@users.noreply.github.com>
2022-12-20 00:47:38 +0000

    heroku starter

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-const PORT = process.env.PORT || 5001
-const http = require('http');
-const hostname = '127.0.0.1';
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World');
-});
-
-server.listen(PORT, hostname, () => {
-    console.log(`Server running at http://${hostname}:${PORT}/`);
-});
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
