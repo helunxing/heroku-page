b1d3c046e88dfb224c6729afdf32f19c2925d165
lx <37706103+helunxing@users.noreply.github.com>
2022-12-23 23:44:00 +0000

    Simple get from microservices URL. I need more knowledge about js language.

--- server/app.js
+++ server/app.js
@@ -1,13 +1,34 @@
 const express = require('express')
 const path = require('path')
+const request = require("request");
 const PORT = process.env.PORT || 5001
 
 app = express()
 
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 
-app.get("/api", (req, res) => {
-    res.json({message: "Hello from server!"});
+app.get("/api", async (req, response) => {
+
+    const HELLO_WORLD_URL = 'http://34.88.110.168:8080/hello-world-bean';
+
+    let res_data = {message: "empty"};
+
+    await request.get({
+        url: HELLO_WORLD_URL,
+        json: true,
+        // headers: {'User-Agent': 'request'}
+    }, (err, res, data) => {
+        if (err) {
+            console.log('Error:', err);
+        } else if (res.statusCode !== 200) {
+            console.log('Status:', res.statusCode);
+        } else {
+            res_data = data;
+            response.json(res_data);
+        }
+        // console.log(res_data);
+    });
+    // console.log(res_data);
 });
 
 app.get('*', (req, res) => {
@@ -16,5 +37,5 @@ app.get('*', (req, res) => {
 
 app.listen(PORT, () => console.log(`Listening on ${PORT}`))
 
-console.log(`test_value is ${process.env.test_value}`)
-console.log(`${process.env}`)
+// console.log(`test_value is ${process.env.test_value}`)
+
