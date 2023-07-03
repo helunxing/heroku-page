0474e63c8f2bc74210a3f759e1d910dad5451e31
lx <37706103+helunxing@users.noreply.github.com>
2023-07-03 23:26:36 +0100

    Update join_detail.js

--- client/src/pages/join_detail.js
+++ client/src/pages/join_detail.js
@@ -6,6 +6,7 @@ import {useParams} from "react-router-dom";
 import styled from "styled-components";
 import {useUtilContext} from "../contexts/util_context";
 import moment from "moment";
+import {Checkbox, FormControl, InputLabel, ListItemText, MenuItem, Select} from "@mui/material";
 
 export default function JoinDetail() {
 
@@ -16,29 +17,59 @@ export default function JoinDetail() {
     const {fetchSingleEvent, single_event} = useEventsContext()
 
     useEffect(() => {
-        if (!logged) {
-            window.location.href = '/login'
-        }
+        // if (!logged) {
+        //     window.location.href = '/login'
+        // }
         document.title = 'Join Event';
         fetchSingleEvent(id);
     }, [])
+
+
+    if (Object.keys(single_event).length === 0) {
+        return <main>
+            <div className={'notice'}>
+                <h2>no event founded</h2>
+            </div>
+        </main>
+    }
+
 
     return <Wrapper>
         <main>
             <table className={'eventTable'}>
                 <tbody>
-                {['title', 'address'].map(
-                    (key) => (<tr>
-                        <td>{key}</td>
-                        <td>{single_event[key]}</td>
-                    </tr>))}
+                {['title', 'date', 'address'].map(
+                    (key) =>
+                        (<tr key={key}>
+                            <td>{key}</td>
+                            <td>{single_event[key]}</td>
+                        </tr>)
+                )}
+                <tr>
+                    <td key={'Options'}>options</td>
+                    <td className={'checkBox'}>
+                        <div>
+                            <FormControl sx={{m: 1, width: 300}}>
+                                <InputLabel id="time-multiple-checkbox-label">Choice all time you like</InputLabel>
+                                <Select
+                                    labelId="time-multiple-checkbox-label"
+                                    id="time-multiple-checkbox"
+                                >
+                                    {single_event['timeOptions'].split(',').map(
+                                        (letter) =>
+                                            (<MenuItem key={letter} value={letter}>
+                                                <Checkbox checked/>
+                                                <ListItemText primary={letter}/>
+                                            </MenuItem>)
+                                    )}
+                                </Select>
+                            </FormControl>
+                        </div>
+                    </td>
+                </tr>
                 </tbody>
             </table>
-            <div className={'eventTable'}>
-                {
 
-                }
-            </div>
         </main>
     </Wrapper>
 }
@@ -50,6 +81,11 @@ const Wrapper = styled.section`
     width: 50%;
     font-size: 2em;
   }
+
+  .checkBox {
+    padding: 0;
+  }
+
   td {
     padding: 10px;
   }
