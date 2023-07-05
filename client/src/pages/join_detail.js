fb80d232fb866a00540adfcc964735478046a537
lx <37706103+helunxing@users.noreply.github.com>
2023-07-05 19:08:59 +0100

    Update join_detail.js

--- client/src/pages/join_detail.js
+++ client/src/pages/join_detail.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import PageHero from "../components/PageHero";
 import EventsList from "../components/EventsList";
 import {useEventsContext} from "../contexts/events_context";
@@ -6,7 +6,8 @@ import {useParams} from "react-router-dom";
 import styled from "styled-components";
 import {useUtilContext} from "../contexts/util_context";
 import moment from "moment";
-import {Checkbox, FormControl, InputLabel, ListItemText, MenuItem, Select} from "@mui/material";
+import {Checkbox, FormControl, InputLabel, ListItemText, MenuItem, OutlinedInput, Select} from "@mui/material";
+import {useJoinContext} from "../contexts/join_context";
 
 export default function JoinDetail() {
 
@@ -15,6 +16,18 @@ export default function JoinDetail() {
     const {logged} = useUtilContext()
 
     const {fetchSingleEvent, single_event} = useEventsContext()
+
+    const [chosenTime, setChosenTime] = useState([]);
+
+    const handleChange = (event) => {
+        const {
+            target: {value},
+        } = event;
+        setChosenTime(
+            typeof value === 'string' ? value.split(",") : value
+        )
+    }
+    // const {} = useJoinContext()
 
     useEffect(() => {
         // if (!logged) {
@@ -33,6 +46,16 @@ export default function JoinDetail() {
         </main>
     }
 
+    const ITEM_HEIGHT = 48;
+    const ITEM_PADDING_TOP = 8;
+    const MenuProps = {
+        PaperProps: {
+            style: {
+                maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+                width: 250,
+            },
+        },
+    };
 
     return <Wrapper>
         <main>
@@ -50,16 +73,27 @@ export default function JoinDetail() {
                     <td className={'checkBox'}>
                         <div>
                             <FormControl sx={{m: 1, width: 300}}>
-                                <InputLabel id="time-multiple-checkbox-label">Choice all time you like</InputLabel>
                                 <Select
                                     labelId="time-multiple-checkbox-label"
                                     id="time-multiple-checkbox"
+                                    multiple
+                                    displayEmpty
+                                    value={chosenTime}
+                                    onChange={handleChange}
+                                    input={<OutlinedInput/>}
+                                    renderValue={(selected) => {
+                                        if (selected.length === 0) {
+                                            return 'Select all time choices you like'
+                                        }
+                                        return selected.join(', ')
+                                    }}
+                                    MenuProps={MenuProps}
                                 >
                                     {single_event['timeOptions'].split(',').map(
-                                        (letter) =>
-                                            (<MenuItem key={letter} value={letter}>
-                                                <Checkbox checked/>
-                                                <ListItemText primary={letter}/>
+                                        (index, value) =>
+                                            (<MenuItem key={value} value={value}>
+                                                <Checkbox checked={chosenTime.indexOf(value) >= 0}/>
+                                                <ListItemText primary={value}/>
                                             </MenuItem>)
                                     )}
                                 </Select>
