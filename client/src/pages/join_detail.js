a6b6aae7e32dbe3eadcb08d7db12673e947b9c56
lx <37706103+helunxing@users.noreply.github.com>
2023-07-20 04:21:22 +0100

    delete useless component

--- client/src/pages/join_detail.js
+++ client/src/pages/join_detail.js
@@ -1,6 +1,5 @@
 import React, {useEffect, useState} from 'react';
 import PageHero from "../components/PageHero";
-import EventsList from "../components/EventsList";
 import {useEventsContext} from "../contexts/events_context";
 import {useParams} from "react-router-dom";
 import styled from "styled-components";
